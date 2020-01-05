25077edca79c79dd0faa4f8b374c08fddafb666f
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2020-01-05 23:32:59 +0900

    Update multi-file components

--- src/js/client.js
+++ src/js/client.js
@@ -1,27 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
-class Layout extends React.Component {
-  constructor(){
-    super();
-    this.name = "yoshi"
-  }
-
-  render() {
-    return (
-      <div>
-        <h1>Hello {this.name} !</h1>
-        {/* コンストラクタから参照 */}
-        <p>1 + 1 = {this.get_result(1)}</p>
-        {/* 関数より参照 */}
-      </div>
-    );
-  }
-
-  get_result(num) {
-    return 1 + num;
-  };
-}
+import Layout from './components/Layout';
 
 const app = document.getElementById('app');
 ReactDOM.render(<Layout />, app);
