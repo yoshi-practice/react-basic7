a6ea159864c93108dd5963ea32ef2160a5b7bd9f
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2020-01-05 23:28:55 +0900

    Update references constructor

--- src/js/client.js
+++ src/js/client.js
@@ -2,15 +2,22 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 class Layout extends React.Component {
+  constructor(){
+    super();
+    this.name = "yoshi"
+  }
+
   render() {
-    let name = 'yoshi';
     return (
       <div>
-        <h1>Hello {name} !</h1>
+        <h1>Hello {this.name} !</h1>
+        {/* コンストラクタから参照 */}
         <p>1 + 1 = {this.get_result(1)}</p>
+        {/* 関数より参照 */}
       </div>
     );
   }
+
   get_result(num) {
     return 1 + num;
   };
